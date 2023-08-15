e94dded6552739cadfa4c868156556bf18b7b0ee
jh <purepleya@gmail.com>
2023-08-15 23:12:43 +0900

    2.16.3. Sources of Arguments 작성 중

---
+++ src/test/java/jh/study/junit/parameterized_test/BasicParameterizedTestDemo.java
@@ -0,0 +1,59 @@
+package jh.study.junit.parameterized_test;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
+import org.junit.platform.commons.util.StringUtils;
+
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+import java.util.EnumSet;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.EnumSource.Mode.EXCLUDE;
+import static org.junit.jupiter.params.provider.EnumSource.Mode.MATCH_ALL;
+
+public class BasicParameterizedTestDemo {
+
+    @ParameterizedTest
+    @ValueSource(ints = { 1, 2, 3 })
+    void testWithValueSource(int argument) {
+        assertTrue(argument > 0 && argument < 4);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = { " ", "   ", "\t", "\n" })
+    void nullEmptyAndBlankStrings(String text) {
+        assertTrue(text == null || text.trim().isEmpty());
+    }
+
+    @ParameterizedTest
+    @EnumSource(ChronoUnit.class)
+    void testWithEnumSource(TemporalUnit unit) {
+        assertNotNull(unit);
+    }
+
+    @ParameterizedTest
+    @EnumSource
+    void testWithEnumSourceWithAutoDetection(ChronoUnit unit) {
+        assertNotNull(unit);
+    }
+
+    @ParameterizedTest
+    @EnumSource(names = { "DAYS", "HOURS" })
+    void testWithEnumSourceInclude(ChronoUnit unit) {
+        assertTrue(EnumSet.of(ChronoUnit.DAYS, ChronoUnit.HOURS).contains(unit));
+    }
+
+    @ParameterizedTest
+    @EnumSource(mode = EXCLUDE, names = { "ERAS", "FOREVER" })
+    void testWithEnumSourceExclude(ChronoUnit unit) {
+        assertFalse(EnumSet.of(ChronoUnit.ERAS, ChronoUnit.FOREVER).contains(unit));
+    }
+
+    @ParameterizedTest
+    @EnumSource(mode = MATCH_ALL, names = "^.*DAYS$")
+    void testWithEnumSourceRegex(ChronoUnit unit) {
+        assertTrue(unit.name().endsWith("DAYS"));
+    }
+}
